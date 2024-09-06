3ff860f28a8967b311be03d953370d22b534d7de
ryouya <ryouya2000@gmail.com>
2024-09-06 22:38:26 +0900

    こみっと

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../styles/page.module.css";
+import nextConfig from "../next.config.mjs";
+import {Footer} from "../components/Footer";
+
+
+const BASE_PATH = nextConfig.basePath || "";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+
+
+
       <main className={styles.main}>
+
+		<div>
+			<img className={styles.icon}  src={`${BASE_PATH}/orbis_icon.png`} alt="orbis_icon"></img>
+		</div> 
         <Image
           className={styles.logo}
           src="https://nextjs.org/icons/next.svg"
@@ -46,50 +58,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <Footer />
     </div>
   );
 }
