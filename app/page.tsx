f66b21a52f03203a3842d09d1ec6ff984967ea67
ryouya <ryouya2000@gmail.com>
2024-09-07 19:19:19 +0900

    ヘッダー仮作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
-import styles from "@/styles/page.module.css";
+import styles from "@/styles/Main.module.css";
 import nextConfig from "@/next.config.mjs";
 import {Header} from "@/components/Header";
 import {Footer} from "@/components/Footer";
+import {Main} from "@/components/Main";
 
 
 const BASE_PATH = nextConfig.basePath || "";
@@ -12,50 +13,9 @@ export default function Home() {
     <div className={styles.page}>
 
     <Header />
-      <main className={styles.main}>
-      <Image
-          src={`${BASE_PATH}/orbis_icon.png`} alt="orbis_icon"
-        width={400}
-              height={400}
-      />
 
-      <div>
-        <strong className={styles.title}>ORBIS RECORD</strong>
-      </div>
+    <Main />
 
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
     <Footer />
     </div>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,25 +3,35 @@ import Link from "next/link";
 import classes from "../styles/Header.module.css";
 import nextConfig from "../next.config.mjs";
 
-
 const BASE_PATH = nextConfig.basePath || "";
 
 export function Header() {
   return (
-      <header className={classes.main}>
-      <Link href="/">
-        <Image
-        src={`${BASE_PATH}/orbis_icon.png`} alt="orbis_icon"
-        width={100}
-        height={100}
-        />
-        ORBIS RECORDdfasd
-      </Link>
+      <header className={classes.header}>
 
-      <Link  href={"/character"}> Character </Link>
-      <Link  href={"/species"}> Species </Link>
-      <Link  href={"/world"}> World </Link>
+        <div  className={classes.headLeft}>
+          <Link href="/" className={classes.logo}>
+            <Image
+            src={`${BASE_PATH}/orbis_icon.png`} alt="orbis_icon"
+            width={90}
+            height={90}
+            />
+            <strong className={classes.logo_text}>ORBIS <br/>RECORD</strong>
+          </Link>
+        </div>
+
+        <div className={classes.head_center}>
+          <Link  href={"/character"}><p className={classes.menu_text}>Character</p></Link>
+          <Link  href={"/species"}><p className={classes.menu_text}>Species</p></Link>
+          <Link  href={"/world"}><p className={classes.menu_text}>World</p></Link>
+          <Link  href={"/items"}><p className={classes.menu_text}>Item</p></Link>
+          <Link  href={"/term"}><p className={classes.menu_text}>Term</p></Link>
+        </div>
+
+        <div className={classes.head_right}>
+          配置検討中
+        </div>
+
       </header>
-
   );
 }
