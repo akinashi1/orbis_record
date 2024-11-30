f51858ecedffd0dcbcae9435db570fd6e1d67713
ryouya <ryouya2000@gmail.com>
2024-11-30 19:27:35 +0900

    デプロイエラー修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header 
-          news={children}
-        />
+        <Header />
         {children}
         <Footer />
       </body>
