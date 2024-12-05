1e5d4399486579a9041b7827215f5f16a6ddb711
ryouya <ryouya2000@gmail.com>
2024-12-05 21:06:41 +0900

    ｆｄさ

--- feautres/top/components/species/SpeciesList.tsx
+++ feautres/top/components/species/SpeciesList.tsx
@@ -10,16 +10,97 @@ export function SpeciesList() {
     <main  className={styles.main}>
       <section  className={styles.species_main}>
         <h1 className={styles.species_title}>種族一覧</h1>
-
-        <h2 className={styles.mini_title}>
-          <div>
-            <span className={styles.mini_title_text}>亜人</span>
-          </div>
-        </h2>
+        
+        <div className={styles.mini_title}>
+          <h2>亜人</h2> 
+          <p>ここに</p>
+        </div>
 
         <div className={styles.species_grid}>
           <div className={styles.grid_item}>
             <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>鬼(西洋鬼)</h2>
+            <p>Ogre</p>
+          </div>
+
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>人狼</h2>
+            <p>英語名</p>
+          </div>
+
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>翼人</h2>
+            <p>英語名</p>
+          </div>
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>鉱人</h2>
+            <p>英語名</p>
+          </div>
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>喰人</h2>
+            <p>英語名</p>
+          </div>
+
+          
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>吸血鬼</h2>
+            <p>英語名</p>
+          </div>
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
+            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
+            width={800} 
+            height={600} 
+            style={{ width: '100%', height: 'auto' }} 
+            />
+            <h2>吸魔鬼</h2>
+            <p>英語名</p>
+          </div>
+          <div className={styles.grid_item}>
+            <Image
+            className={styles.image}
             src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
             width={800} 
             height={600} 
@@ -28,192 +109,28 @@ export function SpeciesList() {
             <h2>種族名</h2>
             <p>英語名</p>
           </div>
-
           <div className={styles.grid_item}>
             <Image
+            className={styles.image}
             src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
             width={800} 
             height={600} 
             style={{ width: '100%', height: 'auto' }} 
             />
             <h2>種族名</h2>
-            <p>説明テキスト</p>
+            <p>英語名</p>
           </div>
-
           <div className={styles.grid_item}>
             <Image
+            className={styles.image}
             src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
             width={800} 
             height={600} 
             style={{ width: '100%', height: 'auto' }} 
             />
             <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-        </div>
-
-        <h2 className={styles.mini_title}>
-          <span className={styles.mini_title_text}>亜人</span>
-        </h2>
-
-        <div className={styles.species_grid}>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-            <h2>種族名</h2>
-            <p>説明テキスト</p>
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
-          <div className={styles.grid_item}>
-            <Image
-            src={`${BASE_PATH}/no_image.png`} alt="orbis_icon"
-            width={800} 
-            height={600} 
-            style={{ width: '100%', height: 'auto' }} 
-            />
-          </div>
-
+            <p>英語名</p>
+          </div>  
         </div>
       </section>
     </main>
